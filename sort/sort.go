63f9e1bbe6d2518b5067c629b425c55c91550015
Yang Yu <yuyangtj@gmail.com>
2018-05-30 20:33:45 +0200

    add merge sort

--- sort/sort.go
+++ sort/sort.go
@@ -14,3 +14,37 @@ func InsertionSort(s []int) []int {
 	return s
 
 }
+
+// merge merges two ordered arrays into a new ordered array
+func merge(A []int, p, q, r int) {
+	L := append([]int(nil), A[p:q]...)
+	R := append([]int(nil), A[q:r]...)
+	i, j := 0, 0
+	for k := p; i < len(L) && j < len(R) && k < r; k++ {
+		if L[i] <= R[j] {
+			A[k] = L[i]
+			i++
+		} else {
+			A[k] = R[j]
+			j++
+		}
+		switch {
+		case i == len(L):
+			copy(A[k+1:], R[j:])
+		case j == len(R):
+			copy(A[k+1:], L[i:])
+		}
+	}
+
+}
+
+//MergeSort implements the inefficient merge sort algorithm
+func MergeSort(A []int, p, r int) []int {
+	if p < r-1 {
+		q := (p + r) / 2
+		MergeSort(A, p, q)
+		MergeSort(A, q, r)
+		merge(A, p, q, r)
+	}
+	return A
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -20,3 +20,19 @@ func TestInsertionSort(t *testing.T) {
 		}
 	}
 }
+
+func TestMergeSort(t *testing.T) {
+	var tests = []struct {
+		input []int
+		want  []int
+	}{
+		{[]int{4, 1, 3, 6}, []int{1, 3, 4, 6}},
+		{[]int{4, 1, 3}, []int{1, 3, 4}},
+	}
+
+	for _, test := range tests {
+		if got := MergeSort(test.input, 0, len(test.input)); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("InsertionSort(%v) = %v", test.input, got)
+		}
+	}
+}
